e4a9520a17be838317d8871b2bb6d4d7ed2a3a15
Mulang Su <sumulang@gmail.com>
2020-10-31 15:21:10 +0800

    added simple table view boilerplate

--- WordFiles/WordListViewController.swift
+++ WordFiles/WordListViewController.swift
@@ -1,5 +1,26 @@
 import UIKit
+import RealmSwift
 
 class WordListViewController : UITableViewController {
+    var words: Results<WordEntry>!
+
+    override func viewDidLoad() {
+        words = DataManager.shared.wordEntries
+
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        words.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = words[indexPath.row].title
+        return cell
+    }
 
 }
