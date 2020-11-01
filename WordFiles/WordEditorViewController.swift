ada3dc97fb8717c6fa181044041c9ed5ece83866
Mulang Su <sumulang@gmail.com>
2020-11-01 11:10:46 +0800

    added title row

--- WordFiles/WordEditorViewController.swift
+++ WordFiles/WordEditorViewController.swift
@@ -1,4 +1,17 @@
 import Eureka
+import SCLAlertView
 
 class WordEditorViewController : FormViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        form +++ TextRow(tagTitle) { row in
+            row.title = "Title"
+            row.value = ""
+        }
+
 }
+
+let tagTitle = "title"
+let tagExplanation = "explanation"
+let tagExample = "example"
