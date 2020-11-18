4562728b691ee6c25fa261026f8b0f45dfde973b
Mulang Su <sumulang@gmail.com>
2020-11-18 21:58:57 +0800

    added multivalued section for tags

--- WordFiles/SentenceEditorViewController.swift
+++ WordFiles/SentenceEditorViewController.swift
@@ -13,6 +13,13 @@ class SentenceEditorViewController : FormViewController {
             row.value = ""
         }
 
+        form +++ MultivaluedSection(multivaluedOptions: [.Delete, .Insert, .Reorder], header: "tags") {
+            $0.addButtonProvider = { section in
+                ButtonRow {
+                    $0.title = "New Tag"
+                }
+            }
+        }
     }
 }
 
