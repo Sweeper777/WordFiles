34b2096213ff041d078f07cc530187f4aa855de6
Mulang Su <sumulang@gmail.com>
2020-11-22 09:46:45 +0800

    added tagsView programmatically in prepareForReuse

--- WordFiles/SentenceCell.swift
+++ WordFiles/SentenceCell.swift
@@ -6,4 +6,12 @@ class SentenceCell : UITableViewCell {
     @IBOutlet var stackView: UIStackView!
     var tagsView: TagsPanelView!
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        stackView.arrangedSubviews[safe: 1]?.removeFromSuperview()
+        tagsView = TagsPanelView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        tagsView.backgroundColor = .clear
+        tagsView.contentMode = .left
+        stackView.addArrangedSubview(tagsView)
+    }
 }
