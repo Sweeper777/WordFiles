0db34ef34c8e60447d78bc50020fb22a09160803
Mulang Su <sumulang@gmail.com>
2020-11-24 21:17:20 +0800

    added table view boilerplate for TagListVC

--- WordFiles/TagListViewController.swift
+++ WordFiles/TagListViewController.swift
@@ -9,5 +9,18 @@ class TagListViewController : UITableViewController {
         tags = DataManager.shared.tags
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tags.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = tags[indexPath.row].name
+        return cell
+    }
 
 }
