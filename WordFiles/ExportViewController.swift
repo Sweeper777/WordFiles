384502f2ec965b7299bb4f1796063c5bb6cc17e2
Mulang Su <sumulang@gmail.com>
2021-01-07 10:45:37 +0800

    added include words row

--- WordFiles/ExportViewController.swift
+++ WordFiles/ExportViewController.swift
@@ -6,6 +6,13 @@ class ExportViewController : FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        form +++ Section("include")
+            <<< SwitchRow(tagWords) {
+                row in
+                row.title = "Words"
+                row.value = true
+            }
+        
     }
 }
 
