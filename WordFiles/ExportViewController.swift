bff893108f291b827b312c7287dc992135f7b901
Mulang Su <sumulang@gmail.com>
2021-01-07 10:45:08 +0800

    Create ExportViewController.swift

---
+++ WordFiles/ExportViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Eureka
+
+class ExportViewController : FormViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+let tagWords = "words"
