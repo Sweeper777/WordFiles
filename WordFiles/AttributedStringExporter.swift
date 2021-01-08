c784e3aebf83b50822111b8e20a00f910caf4096
Mulang Su <sumulang@gmail.com>
2021-01-08 08:46:01 +0800

    added attribute dictionaries for each of the styles used

--- WordFiles/AttributedStringExporter.swift
+++ WordFiles/AttributedStringExporter.swift
@@ -27,4 +27,33 @@ struct AttributedStringExporter {
     private let bigFont = UIFont.monospacedSystemFont(ofSize: 16, weight: .bold)
     private let regularFont = UIFont.monospacedSystemFont(ofSize: 11, weight: .regular)
     
+    private var titleAttributes: [NSAttributedString.Key: Any] {
+        [
+            .font: bigFont,
+            .paragraphStyle: alignCenter
+        ]
+    }
+    
+    private var headingsAttributes: [NSAttributedString.Key: Any] {
+        [
+            .font: boldFont,
+            .paragraphStyle: alignLeft
+        ]
+    }
+    
+    private var bodyAttributes: [NSAttributedString.Key: Any] {
+        [
+            .font: regularFont,
+            .paragraphStyle: alignLeft
+        ]
+    }
+    
+    private var tagsAttributes: [NSAttributedString.Key: Any] {
+        [
+            .font: regularFont,
+            .paragraphStyle: alignLeft,
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+    }
+    
 }
