a31290927aa86ccff096d986b5f314571f419c69
Mulang Su <sumulang@gmail.com>
2021-01-09 10:10:53 +0800

    added Export to PDF button with dummy implementation

--- WordFiles/ExportViewController.swift
+++ WordFiles/ExportViewController.swift
@@ -19,6 +19,20 @@ class ExportViewController : FormViewController {
                 row.value = true
             }
         
+        form +++ ButtonRow() {
+            row in
+            row.title = "Export to PDF"
+        }.onCellSelection({ (cell, row) in
+            let attributedString = AttributedStringExporter(options: [.includeSentences, .includeWords]).attributedStringFromAppData()
+            let data = AttributedTextToPDFConverter().pdfWithText(attributedString)
+            do {
+                let url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("test.pdf")
+                try data.write(to: url, options: .atomic)
+                print("Written PDF file to \(url)")
+            } catch {
+                print(error)
+            }
+        })
     }
 }
 
