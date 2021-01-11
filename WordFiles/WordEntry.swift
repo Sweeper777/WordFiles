185121fc60df1c139fc974e395560658b4e1836a
Mulang Su <sumulang@gmail.com>
2021-01-11 19:33:59 +0800

    added WordTags realm object

--- WordFiles/WordEntry.swift
+++ WordFiles/WordEntry.swift
@@ -4,4 +4,15 @@ class WordEntry : Object {
     @objc dynamic var title = ""
     @objc dynamic var explanation = ""
     @objc dynamic var example = ""
+    
+    let tags = List<WordTag>()
+}
+
+class WordTag : Object {
+    @objc dynamic var name = ""
+    let words = LinkingObjects(fromType: WordEntry.self, property: "tags")
+
+    override class func primaryKey() -> String? {
+        "name"
+    }
 }
