1a976888e55d110b53fb0d3d9175c8ee63566d7f
Mulang Su <sumulang@gmail.com>
2021-06-03 09:22:45 +0800

    added tagsViewHeightConstraint

--- WordFiles/TextWithTagCell.swift
+++ WordFiles/TextWithTagCell.swift
@@ -5,13 +5,17 @@ class TextWithTagCell : UITableViewCell {
     @IBOutlet var contentLabel: UILabel!
     @IBOutlet var stackView: UIStackView!
     var tagsView: TagsPanelView!
+    var tagsViewHeightConstraint: NSLayoutConstraint!
 
     override func prepareForReuse() {
         super.prepareForReuse()
         stackView.arrangedSubviews[safe: 1]?.removeFromSuperview()
         tagsView = TagsPanelView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        tagsView.translatesAutoresizingMaskIntoConstraints = false
         tagsView.backgroundColor = .clear
         tagsView.contentMode = .left
+        tagsViewHeightConstraint = tagsView.heightAnchor.constraint(equalToConstant: 100)
+        tagsViewHeightConstraint.isActive = true
         stackView.addArrangedSubview(tagsView)
     }
 
